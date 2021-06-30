9054274d9e307c4b8db09eb5dbd1c9c97ddb5a79
zhaocx <835844534@qq.com>
2021-06-30 17:33:36 +0800

    增加了一个成员变量

--- TestGit1/src/cn/itcast/Demo/Demo1.java
+++ TestGit1/src/cn/itcast/Demo/Demo1.java
@@ -1,9 +1,11 @@
 package cn.itcast.Demo;
 
 public class Demo1 {
+    private String Demo1;
     public static void main(String[] args) {
         System.out.println("测试用Demo");
         System.out.println("测试用Demo");
         System.out.println("测试用Demo");
     }
+
 }
