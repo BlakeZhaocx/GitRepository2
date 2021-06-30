55769be070c2f384a62938a8b0fa2d2ab65f65ad
zhaocx <835844534@qq.com>
2021-06-30 17:31:40 +0800

    第一次通过idea将测试代码提交到本地仓库;

--- TestGit1/src/cn/itcast/Demo/Demo1.java
+++ TestGit1/src/cn/itcast/Demo/Demo1.java
@@ -3,5 +3,7 @@ package cn.itcast.Demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("测试用Demo");
+        System.out.println("测试用Demo");
+        System.out.println("测试用Demo");
     }
 }
